c77e8c726e0423a77c1f7f1dec710305bf995b19
Paweł Kamiński <pkaminski@hitduck.com>
2021-02-20 21:26:43 +0100

    feat(quote_server): add quote server dockerfile

---
+++ market_data_client/src/main/java/al/assignment/MarkedDataClient.java
@@ -0,0 +1,10 @@
+package al.assignment;
+
+public class MarkedDataClient {
+
+    public static void main(String[] args) {
+        // mock run
+        System.out.println("TEST OUTPUT");
+    }
+
+}
